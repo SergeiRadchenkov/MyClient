1c47fcc44f378d621c01c4a8ab622698a401704e
SergeiRadchenkov <radchenkovsa@yandex.ru>
2025-02-10 23:29:51 +0300

    Final

--- MyClientProject/MyClientProject/settings.py
+++ MyClientProject/MyClientProject/settings.py
@@ -21,14 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--%($xr*k=b%mlf=x+z@r0vr5x2k%7^xq89r=rgkkj0*(u9q*3i'
+''' SECRET_KEY = 'django-insecure--%($xr*k=b%mlf=x+z@r0vr5x2k%7^xq89r=rgkkj0*(u9q*3i' '''
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
+    'sergeiradchenkov.pythonanywhere.com',
 ]
 
 # Application definition
@@ -79,8 +83,15 @@ WSGI_APPLICATION = 'MyClientProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sergeiradchenkov$default',
+        'USER': 'sergeiradchenkov',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'sergeiradchenkov.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
